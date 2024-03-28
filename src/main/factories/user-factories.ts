df2bf848487cf9881e042392aa3026aeaf310ffd
lucas vinicius <lucassilved@gmail.com>
2024-03-28 20:59:09 -0300

    feat: create factory to create user controller

---
+++ src/main/factories/user-factories.ts
@@ -0,0 +1,10 @@
+import { CreateUser } from '../../application/use-case/create-user'
+import { IUserRepository } from '../../application/repositories/iuser-repository'
+import { CreateUserController } from '../../infrastructure/express/controllers/create-user-controller'
+
+export function createUserControllerFactory() {
+  let userRepository: IUserRepository
+  const createUser = new CreateUser(userRepository)
+  const createUserController = new CreateUserController(createUser)
+  return createUserController.handle
+}
