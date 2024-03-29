378e7b3533ef32060fed7eb30ff1edf529a4ccba
lucas vinicius <lucassilved@gmail.com>
2024-03-29 18:13:29 -0300

    feat: add password factory

---
+++ src/main/factories/password-factory.ts
@@ -0,0 +1,8 @@
+import { PBKDF2Password } from '../../domain/pbkdf2-password'
+
+export type MakePasswordOutput = typeof PBKDF2Password
+
+export function makePassword(algorithm: string): MakePasswordOutput {
+  if (algorithm === 'PBKDF2') return PBKDF2Password
+  throw new Error('algorithm used in the password is not implemented')
+}
