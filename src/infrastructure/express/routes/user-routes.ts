6e11b6f8f0c737f158ca79dc56e428a8dae9fa71
lucas vinicius <lucassilved@gmail.com>
2024-04-02 21:44:30 -0300

    feat: add get user of id route and controller factory

--- src/infrastructure/express/routes/user-routes.ts
+++ src/infrastructure/express/routes/user-routes.ts
@@ -1,8 +1,14 @@
 import { Router, type Request, type Response } from 'express'
-import { createUserControllerFactory } from '../../../main/factories/user-factories'
+import {
+  createUserControllerFactory,
+  getUserOfIdControllerFactory,
+} from '../../../main/factories/user-factories'
 
 const userRoutes = Router()
 userRoutes.post('/', (request: Request, response: Response) => {
   createUserControllerFactory().handle(request, response)
+})
+userRoutes.get('/:id', (request: Request, response: Response) => {
+  getUserOfIdControllerFactory().handle(request, response)
 })
 export { userRoutes }
--- src/main/factories/user-factories.ts
+++ src/main/factories/user-factories.ts
@@ -1,5 +1,7 @@
 import { CreateUser } from '../../application/use-case/create-user'
+import { GetUserOfId } from '../../application/use-case/get-user-of-id'
 import { CreateUserController } from '../../infrastructure/express/controllers/create-user-controller'
+import { GetUserOfIdController } from '../../infrastructure/express/controllers/get-user-of-id-controller'
 import { IController } from '../../infrastructure/express/controllers/icontoller'
 import { PrismaUserRepository } from '../../infrastructure/repositories/prisma/prisma-user-repository'
 
@@ -8,4 +10,9 @@ const userRepository = new PrismaUserRepository()
 export function createUserControllerFactory(): IController {
   const createUser = new CreateUser(userRepository)
   return new CreateUserController(createUser)
+}
+
+export function getUserOfIdControllerFactory(): IController {
+  const getUserOfId = new GetUserOfId(userRepository)
+  return new GetUserOfIdController(getUserOfId)
 }
