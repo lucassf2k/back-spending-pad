f377399c1ef12ecb4bc0bc7d4b5868c5f001c8f7
lucas vinicius <lucassilved@gmail.com>
2024-04-03 08:51:00 -0300

    fix: adiciona api error quando a transação não existe na hora de deletar

--- src/application/use-case/delete-transaction.ts
+++ src/application/use-case/delete-transaction.ts
@@ -1,3 +1,5 @@
+import { ApiError } from '../../common/api-error'
+import { StatusCode } from '../../common/status-code'
 import { DeleteTransactionDTO } from '../../infrastructure/dtos/delete-transaction-dto'
 import { ITransactionRepository } from '../repositories/itransaction-repository'
 
@@ -5,6 +7,12 @@ export class DeleteTransaction {
   constructor(private readonly transactionRepository: ITransactionRepository) {}
 
   async execute(input: DeleteTransactionDTO) {
+    const transactionAlreadyExistis = await this.transactionRepository.get(
+      input.id,
+    )
+    if (!transactionAlreadyExistis) {
+      throw new ApiError('transação não encontrada', StatusCode.NOT_FOUND)
+    }
     return this.transactionRepository.delete(input.id)
   }
 }
--- src/infrastructure/express/controllers/delete-transaction-controller.ts
+++ src/infrastructure/express/controllers/delete-transaction-controller.ts
@@ -1,10 +1,8 @@
 import type { Request, Response } from 'express'
 import { IController } from './icontoller'
-import { DeleteTransaction } from '../../../application/use-case/delete-transaction'
-import { DeleteTransactionValidation } from '../../dtos/delete-transaction-dto'
-import { ApiError } from '../../../common/api-error'
-import { StatusCode } from '../../../common/status-code'
 import { errorHandler } from '../middlewares/error-handler'
+import { DeleteTransactionValidation } from '../../dtos/delete-transaction-dto'
+import { DeleteTransaction } from '../../../application/use-case/delete-transaction'
 
 export class DeleteTransactionController implements IController {
   constructor(private readonly deleteTransaction: DeleteTransaction) {}
@@ -13,9 +11,6 @@ export class DeleteTransactionController implements IController {
     try {
       const input = DeleteTransactionValidation.parse(request.params)
       const transaction = await this.deleteTransaction.execute(input)
-      if (!transaction) {
-        throw new ApiError('transação não encontrada', StatusCode.NOT_FOUND)
-      }
       return response.json({ id: transaction._id })
     } catch (error) {
       errorHandler(error, request, response)
