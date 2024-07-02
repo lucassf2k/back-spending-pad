93f04ed66692af4f5fc33c35d848a9c905ab221f
lucas vinicius <lucassilved@gmail.com>
2024-07-02 17:16:05 -0300

    feat: add main with cluster

---
+++ src/main-cluster.ts
@@ -0,0 +1,25 @@
+import { availableParallelism } from 'node:os';
+import Cluster from 'node:cluster';
+import { ExpressApplication } from './infrastructure/express';
+
+const cpuAvailableCount = availableParallelism() - 1;
+
+function runPrimaryProcess() {
+  console.log(`Numbers of CPUs is ${cpuAvailableCount}`);
+  console.log(`Primary ${process.pid} is running...`);
+  for (let index = 0; index < cpuAvailableCount; index++) {
+    Cluster.fork();
+  }
+  Cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died!`);
+    console.log(`Let's fork another worker...`);
+    Cluster.fork();
+  });
+}
+
+function runWorkerProcess() {
+  console.log(`Ẁorker ${process.pid} started...`);
+  ExpressApplication();
+}
+
+Cluster.isPrimary ? runPrimaryProcess() : runWorkerProcess();
