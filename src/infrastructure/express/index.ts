c0f0297e183d6b4f05dbc61f4398786a5f00046d
lucas vinicius <lucassilved@gmail.com>
2025-02-26 19:38:04 -0300

    feat: add helmet

--- src/infrastructure/express/index.ts
+++ src/infrastructure/express/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import helmet from 'helmet';
 import { routes } from '../../infrastructure/express/routes';
 import { ENV } from '../../infrastructure/configurations/environments';
 import { swaggerService } from '../../infrastructure/services/swagger-service';
@@ -10,6 +11,7 @@ export function ExpressApplication() {
   const app = express();
   app.use(express.json());
   app.use(cors());
+  app.use(helmet());
   swaggerService(app);
   app.use('/api', routes);
   app.use(errorHandler);
